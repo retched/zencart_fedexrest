2533d2b9e3ccde138a70eac9c83ba7fc7cedd8c3
Scott Wilson <scottcwilson@hey.com>
2024-04-11 13:15:32 -0400

    Used wrong files last checkin

--- includes/languages/english/modules/shipping/fedexrest.php
+++ includes/languages/english/modules/shipping/fedexrest.php
@@ -1,7 +1,7 @@
 <?php
 // Language file for FedEx REST API 
-define('MODULE_SHIPPING_FEDEX_REST_TEXT_TITLE', 'FedEx REST API');
-define('MODULE_SHIPPING_FEDEX_REST_TEXT_DESCRIPTION', '<h2>FedEx REST API</h2><p>You will need to have registered an account with FedEx to use this module. Please see the README.TXT file.</p>');
+define('MODULE_SHIPPING_FEDEX_REST_TEXT_TITLE', 'FedEx'); 
+define('MODULE_SHIPPING_FEDEX_REST_TEXT_DESCRIPTION', '<h2>FedEx REST API</h2><p>You will need to have registered an account with FedEx to use this module. Please see the README.md file.</p>');
 
 // For locales other than en_US please see 
 // https://developer.fedex.com/api/en-mx/guides/api-reference.html#locales
